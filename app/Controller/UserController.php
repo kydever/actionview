fc8c06cd69853c6eda4b48a770081f9e743efd69
李铭昕 <715557344@qq.com>
2021-11-26 11:18:31 +0800

    完善用户登录接口

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -12,11 +12,28 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Request\SessionCreateRequest;
+use App\Service\Formatter\UserFormatter;
+use App\Service\UserService;
+use Hyperf\Di\Annotation\Inject;
 
 class UserController extends Controller
 {
+    #[Inject]
+    protected UserService $service;
+
+    #[Inject]
+    protected UserFormatter $formatter;
+
     public function login(SessionCreateRequest $request)
     {
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = $this->service->login($email, $password);
+
+        return $this->response->success(
+            $this->formatter->base($user)
+        );
     }
 
     public function register()
