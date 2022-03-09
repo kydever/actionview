92e5cb5a199cb316ea12d277fce8e53cd92fbd73
kydev <kydev@github.com>
2022-03-09 14:25:59 +0800

    代码优化

--- app/Service/Formatter/TypeFormatter.php
+++ app/Service/Formatter/TypeFormatter.php
@@ -12,11 +12,15 @@ declare(strict_types=1);
 namespace App\Service\Formatter;
 
 use App\Model\ConfigType;
+use App\Service\StateService;
 use Han\Utils\Service;
 use Hyperf\Database\Model\Collection;
 
 class TypeFormatter extends Service
 {
+    #[Inject]
+    protected ProviderService $provider;
+
     public function base(ConfigType $model)
     {
         return [
@@ -31,11 +35,20 @@ class TypeFormatter extends Service
         ];
     }
 
-    public function formatList(Collection $models)
+    /**
+     * @param Collection<int, ConfigType> $models
+     */
+    public function formatList(Collection $models, bool $withIsUsed = false)
     {
-        $results = [];
+        $result = [];
         foreach ($models as $model) {
-            $results[] = $this->base($model);
+            $item = $this->base($model);
+            $item['is_used'] = di()->get(StateService::class)->isFieldUsedByIssue($model->project_key);
+        }
+        $results = [];
+        $stateService = di()->get(StateService::class);
+        foreach ($models as $model) {
+            $model->is_used = $stateService->isFieldUsedByIssue($project, 'type', $model->toArray());
         }
 
         return $results;
