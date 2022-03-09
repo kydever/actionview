df1d67174620416822dee99695477581f04515eb
kydev <tech@knowyourself.cc>
2022-03-09 14:35:42 +0800

    删除无用代码

--- app/Service/Formatter/TypeFormatter.php
+++ app/Service/Formatter/TypeFormatter.php
@@ -12,15 +12,10 @@ declare(strict_types=1);
 namespace App\Service\Formatter;
 
 use App\Model\ConfigType;
-use App\Service\StateService;
 use Han\Utils\Service;
-use Hyperf\Database\Model\Collection;
 
 class TypeFormatter extends Service
 {
-    #[Inject]
-    protected ProviderService $provider;
-
     public function base(ConfigType $model)
     {
         return [
@@ -33,24 +28,5 @@ class TypeFormatter extends Service
             'description' => $model->description,
             'disabled' => $model->disabled,
         ];
-    }
-
-    /**
-     * @param Collection<int, ConfigType> $models
-     */
-    public function formatList(Collection $models, bool $withIsUsed = false)
-    {
-        $result = [];
-        foreach ($models as $model) {
-            $item = $this->base($model);
-            $item['is_used'] = di()->get(StateService::class)->isFieldUsedByIssue($model->project_key);
-        }
-        $results = [];
-        $stateService = di()->get(StateService::class);
-        foreach ($models as $model) {
-            $model->is_used = $stateService->isFieldUsedByIssue($project, 'type', $model->toArray());
-        }
-
-        return $results;
     }
 }
