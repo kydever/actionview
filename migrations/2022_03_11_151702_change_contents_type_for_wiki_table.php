86e3e2ee4865762f80a705e40d6c7786832e765d
kydev <tech@knowyourself.cc>
2022-03-11 15:18:51 +0800

    更改 contents 的类型

---
+++ migrations/2022_03_11_151702_change_contents_type_for_wiki_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+use Hyperf\Database\Migrations\Migration;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Schema\Schema;
+use Hyperf\DbConnection\Db;
+
+class ChangeContentsTypeForWikiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('', function (Blueprint $table) {
+            Db::select('ALTER TABLE `wiki` CHANGE `contents` `contents` TEXT  CHARACTER SET utf8mb4  COLLATE utf8mb4_unicode_ci  NOT NULL;');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('', function (Blueprint $table) {
+        });
+    }
+}
