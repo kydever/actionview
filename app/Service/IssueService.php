aa6456ae2f22622c00d562bdac47987f700a215f
李铭昕 <715557344@qq.com>
2022-04-09 15:20:12 +0800

    代码优化

--- app/Service/IssueService.php
+++ app/Service/IssueService.php
@@ -1140,65 +1140,7 @@ class IssueService extends Service
                 continue;
             }
 
-            if (isset($data['labels'])) {
-                $labels = $data['labels'] ? array_values($data['labels']) : [];
-                unset($data['labels']);
-                $model->fillJsonAttribute('data->labels', $labels);
-            }
-
-            if (isset($data['descriptions'])) {
-                $descriptions = $data['descriptions'];
-                unset($data['descriptions']);
-                $model->fillJsonAttribute('data->descriptions', $descriptions);
-            }
-
-            if (isset($data['comments'])) {
-                $comments = $data['comments'];
-                unset($data['comments']);
-                $model->fillJsonAttribute('data->comments', $comments);
-            }
-
-            if (isset($data['related_users'])) {
-                $relatedUsers = $data['related_users'] ? array_values($data['related_users']) : [];
-                unset($data['related_users']);
-                $model->fillJsonAttribute('data->related_users', $relatedUsers);
-            }
-
-            if (isset($data['progress'])) {
-                $progress = $data['progress'];
-                unset($data['progress']);
-                $model->fillJsonAttribute('data->progress', $progress);
-            }
-
-            if (isset($data['priority'])) {
-                $priority = $data['priority'];
-                unset($data['priority']);
-                $model->fillJsonAttribute('data->priority', $priority);
-            }
-
-            if (isset($data['expect_complete_time'])) {
-                $expectCompleteTime = $data['expect_complete_time'];
-                unset($data['expect_complete_time']);
-                $model->fillJsonAttribute('data->expect_complete_time', $expectCompleteTime);
-            }
-
-            if (isset($data['original_estimate'])) {
-                $originalEstimate = $data['original_estimate'];
-                unset($data['original_estimate']);
-                $model->fillJsonAttribute('data->original_estimate', $originalEstimate);
-            }
-
-            if (isset($data['story_points'])) {
-                $storyPoints = $data['story_points'];
-                unset($data['story_points']);
-                $model->fillJsonAttribute('data->story_points', $storyPoints);
-            }
-
-            if (isset($data['expect_start_time'])) {
-                $expectStartTime = $data['expect_start_time'];
-                unset($data['expect_start_time']);
-                $model->fillJsonAttribute('data->expect_start_time', $expectStartTime);
-            }
+            $data = $this->fillIssueJsonAttribute($model, $data);
 
             $model->fill($data)->save();
             $model->pushToSearch();
@@ -1210,6 +1152,71 @@ class IssueService extends Service
         }
 
         return ['ids' => $ids];
+    }
+
+    protected function fillIssueJsonAttribute(Issue $model, array $data): array
+    {
+        if (isset($data['labels'])) {
+            $labels = $data['labels'] ? array_values($data['labels']) : [];
+            unset($data['labels']);
+            $model->fillJsonAttribute('data->labels', $labels);
+        }
+
+        if (isset($data['descriptions'])) {
+            $descriptions = $data['descriptions'];
+            unset($data['descriptions']);
+            $model->fillJsonAttribute('data->descriptions', $descriptions);
+        }
+
+        if (isset($data['comments'])) {
+            $comments = $data['comments'];
+            unset($data['comments']);
+            $model->fillJsonAttribute('data->comments', $comments);
+        }
+
+        if (isset($data['related_users'])) {
+            $relatedUsers = $data['related_users'] ? array_values($data['related_users']) : [];
+            unset($data['related_users']);
+            $model->fillJsonAttribute('data->related_users', $relatedUsers);
+        }
+
+        if (isset($data['progress'])) {
+            $progress = $data['progress'];
+            unset($data['progress']);
+            $model->fillJsonAttribute('data->progress', $progress);
+        }
+
+        if (isset($data['priority'])) {
+            $priority = $data['priority'];
+            unset($data['priority']);
+            $model->fillJsonAttribute('data->priority', $priority);
+        }
+
+        if (isset($data['expect_complete_time'])) {
+            $expectCompleteTime = $data['expect_complete_time'];
+            unset($data['expect_complete_time']);
+            $model->fillJsonAttribute('data->expect_complete_time', $expectCompleteTime);
+        }
+
+        if (isset($data['original_estimate'])) {
+            $originalEstimate = $data['original_estimate'];
+            unset($data['original_estimate']);
+            $model->fillJsonAttribute('data->original_estimate', $originalEstimate);
+        }
+
+        if (isset($data['story_points'])) {
+            $storyPoints = $data['story_points'];
+            unset($data['story_points']);
+            $model->fillJsonAttribute('data->story_points', $storyPoints);
+        }
+
+        if (isset($data['expect_start_time'])) {
+            $expectStartTime = $data['expect_start_time'];
+            unset($data['expect_start_time']);
+            $model->fillJsonAttribute('data->expect_start_time', $expectStartTime);
+        }
+
+        return $data;
     }
 
     public function doAction(int $id, int $workflowId, array $input, User $user, Project $project)
