9b92ec0ba6d6d78d7561043bcd3c868e4cdb4a2b
laradocs <75692074@qq.com>
2022-04-19 22:37:32 +0800

    Format

--- app/Service/GroupService.php
+++ app/Service/GroupService.php
@@ -167,16 +167,16 @@ class GroupService extends Service
         return $this->dao->likeByName(
             $s,
             fn ($query) => $query->where(
-            fn ($query) => $query->where('principal->id', get_user_id())
-                    ->orWhere(
-                        fn ($query) => $query->where('public_scope', 3)
+                fn ($query) => $query->where('principal->id', get_user_id())
+                ->orWhere(
+                    fn ($query) => $query->where('public_scope', 3)
                             ->where('users', get_user_id())
-                    )
-                    ->orWhere(
-                        fn ($query) => $query->where('public_scope', '!=', 2)
+                )
+                ->orWhere(
+                    fn ($query) => $query->where('public_scope', '!=', 2)
                             ->where('public_scope', '!=', 3)
-                    )
-        )->get(['id', 'name'])
+                )
+            )->get(['id', 'name'])
         );
     }
 }
